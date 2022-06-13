e6c565205b7be3b0db6b64794351e283ddede537
Christian Leingang <chrilei2000@gmail.com>
2022-06-13 15:51:04 +0200

    update init

---
+++ lib/labeled_textfield.dart
@@ -0,0 +1,37 @@
+library labeled_textfield;
+
+import 'package:flutter/material.dart';
+
+class LabeledTextfield extends StatelessWidget {
+  String description;
+  String value;
+  Color lineColor;
+
+  LabeledTextfield(
+      {Key? key, required this.description, required this.value, required this.lineColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            description,
+            style: const TextStyle(fontSize: 16),
+          ),
+          Text(
+            value,
+            style: const TextStyle(fontSize: 20),
+          ),
+          Divider(
+            color: lineColor.withOpacity(0.7),
+            thickness: 1,
+          ),
+        ],
+      ),
+    );
+  }
+}
